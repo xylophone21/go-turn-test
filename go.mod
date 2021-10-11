c820ee212aa8cb152b72ea41d08209a40d66bcaf
Hui Li <xylophone21@gmail.com>
2021-10-11 11:36:48 +0800

    init project

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/xylophone21/go-turn-test
+
+go 1.14
+
+require (
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+// Package main go 实现的压测工具
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("hello")
+	return
+}
