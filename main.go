412e6dc67f7160363c86409ad848f6656c530179
Hui Li <xylophone21@gmail.com>
2021-10-17 01:34:25 +0800

    added main

--- main.go
+++ main.go
@@ -2,10 +2,83 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"time"
+
+	"github.com/pion/logging"
+	"github.com/xylophone21/go-turn-test/dispose"
 )
 
+var (
+	connections  uint64        = 5
+	duration     time.Duration = time.Second * 30
+	packageSize  int           = 1024
+	packageWait  time.Duration = time.Second
+	statLogLvl   int           = int(logging.LogLevelInfo)
+	reqLogLvl    int           = int(logging.LogLevelWarn)
+	is2CloudMode bool          = false
+	isAwsMode    bool          = false
+	stunServer   string        = ""
+	turnServer   string        = "hellohui.space:3478"
+	username     string        = ""
+	password     string        = ""
+)
+
+func init() {
+	flag.Uint64Var(&connections, "c", connections, "Number of TURN connections")
+	flag.DurationVar(&duration, "d", duration, "Duration of test")
+	flag.IntVar(&packageSize, "s", packageSize, "Package size to send")
+	flag.DurationVar(&packageWait, "w", packageWait, "Duration per each send")
+	flag.IntVar(&statLogLvl, "statlog", statLogLvl, "Log level of statistics")
+	flag.IntVar(&reqLogLvl, "reqlog", reqLogLvl, "Log level of request")
+	flag.BoolVar(&is2CloudMode, "t", is2CloudMode, "Using cloud2cloud turn mode")
+	flag.BoolVar(&isAwsMode, "a", isAwsMode, "Using AWS turn server")
+	flag.StringVar(&stunServer, "stun", stunServer, "Stun server url")
+	flag.StringVar(&turnServer, "turn", stunServer, "Turn server url")
+	flag.StringVar(&username, "u", username, "Username of turn server")
+	flag.StringVar(&password, "p", password, "Password of turn server")
+
+	// 解析参数
+	flag.Parse()
+}
+
 func main() {
-	fmt.Print("hello")
-	return
+	req := &dispose.DisposeRequestST{
+		ChanCount:      connections,
+		Duration:       duration,
+		PackageSize:    int32(packageSize),
+		PackageWait:    packageWait,
+		StatLogLvl:     statLogLvl,
+		ReqLogLvl:      reqLogLvl,
+		StunServerAddr: stunServer,
+		TurnServerAddr: turnServer,
+		Username:       username,
+		Password:       password,
+	}
+
+	var mode string
+	if is2CloudMode {
+		req.Mode = dispose.MODE_2CLOUD
+		mode = "2 cloud mode"
+	} else {
+		req.Mode = dispose.MODE_1CLOUD
+		mode = "1 cloud mode"
+	}
+
+	var server string
+	if isAwsMode {
+		req.Source = dispose.SOURCE_AWS
+		server = turnServer
+	} else {
+		req.Source = dispose.SOURCE_BASE
+		server = "{get from aws}"
+	}
+
+	fmt.Printf("Start request %v connections to %v by %v\n", connections, server, mode)
+
+	err := dispose.Dispose(req)
+	if err != nil {
+		fmt.Printf("Run error:%v\n", err)
+	}
 }
